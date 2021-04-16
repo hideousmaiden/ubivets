73a2a880b8ab1b23d9df40c64401f6c00c0774b8
a.tulubieva <71927278+tuluqeva@users.noreply.github.com>
2021-04-16 18:19:23 +0400

    Update tel_start.py

--- tel_start.py
+++ tel_start.py
@@ -4,6 +4,7 @@ import csv
 import httplib2
 import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials
+import networkx as nx
 
 CREDENTIALS_FILE = 'cybersep-310108-c1268b1fb570.json'
 
@@ -33,6 +34,128 @@ def status_writer(id, status):
 def add_friend(chat_id, text):
     well = 'ДОПИСАТЬ ПОЛУЧЕННЫЙ ТЕКСТ В ФАЙЛ В СПИСОК ЗНАКОМЦЕВ ПОЛЬЗОВАТЕЛЯ'
 
+def separator(shit_id):
+    result = service.spreadsheets().values().get(spreadsheetId=shit_id, range="A1:A500").execute()
+    rows = result.get('values')
+    length = len(rows) #количество строчек в таблице
+    edges = []
+    isolat = []
+    column_name = 'C' #столбец с именами
+    column_conn = 'E' #столбец со знакомыми
+    column_vict = 'F' #колонка с жертвами
+    column_stat = 'D' #колонка со статусами
+
+    for number in range(2,length+1):
+        cell_name = column_name + str(number)
+        result = service.spreadsheets().values().get(spreadsheetId=shit_id, range=cell_name).execute()
+        name = result.get('values')
+        cell_stat = column_stat + str(number)
+        result = service.spreadsheets().values().get(
+        spreadsheetId=shit_id, range=cell_stat).execute()
+        stat = result.get('values')
+        cell_conn = column_conn + str(number)
+        result = service.spreadsheets().values().get(
+        spreadsheetId=shit_id, range=cell_conn).execute()
+        conn = result.get('values')
+        if stat[0][0] == 'Участник':
+            if conn != None:
+                links = conn[0][0].split(';')
+                for elem in links:
+                    link = []
+                    link.append(name[0][0])
+                    link.append(elem)
+                    edges.append(tuple(link))
+            else:
+                isolat.append(name[0][0])
+
+#графы
+    final_cycles = []
+    weighted_edges = []
+    mutual_edges = []
+    limit = 0
+    raw_graph = nx.DiGraph()
+    raw_graph.add_edges_from(edges)
+    raw_graph.add_nodes_from(isolat)
+    nodes = raw_graph.nodes
+    for edge_number in range(len(edges)):
+        rev_edge = list(edges[edge_number])
+        rev_edge.reverse()
+        if tuple(rev_edge) not in edges:
+             raw_graph.remove_edge(edges[edge_number][0], edges[edge_number][1])
+    paths = nx.shortest_path(raw_graph)
+    no_conn_rate = len(nodes) + 1
+    weighted_edges = []
+    for start in paths:
+        for target in nodes:
+            if start != target:
+                if target in paths[start]:
+                    weight = len(paths[start][target])
+                else:
+                    weight = no_conn_rate
+                if weight > limit:
+                    weighted_edge = []
+                    weighted_edge.append(start)
+                    weighted_edge.append(target)
+                    weighted_edge.append(weight)
+                    weighted_edges.append(tuple(weighted_edge))
+    weighted_graph = nx.DiGraph()
+    weighted_graph.add_weighted_edges_from(weighted_edges)
+    final_paths =  weighted_graph.adj
+    cycles = list(nx.simple_cycles(weighted_graph))
+    if len(cycles) == 0:
+        print('измените значение limit')
+    else:
+        for cycle in cycles:
+            if len(cycle) == len(nodes):
+                reversed = []
+                reversed.append(cycle[0])
+                part = cycle[1:len(cycle)]
+                part.reverse()
+                reversed.extend(part)
+                if reversed not in final_cycles:
+                    final_cycles.append(cycle)
+        max_count = 0
+        for cycle in final_cycles:
+            cycle_count = 0
+            for n_number in range(len(nodes) - 1):
+                start = cycle[n_number]
+                target = cycle[n_number + 1]
+                cycle_count += final_paths[start][target]['weight']
+            cycle_count += final_paths[cycle[-1]][cycle[0]]['weight']
+            if cycle_count > max_count:
+                max_count = cycle_count
+                best_cycle = cycle
+#запись в фаил
+    for number in range(2,length+1):
+        cell_name = column_name + str(number)
+        result = service.spreadsheets().values().get(
+        spreadsheetId=shit_id, range=cell_name).execute()
+        name = result.get('values')
+        cell_vict = column_vict + str(number)
+        for num in range(len(best_cycle)):
+            if name[0][0] == best_cycle[num] and num != (len(best_cycle) - 1):
+                empt_l = []
+                empt_ll = []
+                empt_l.append(best_cycle[num+1])
+                empt_ll.append(empt_l)
+                results = service.spreadsheets().values().batchUpdate(spreadsheetId = shit_id, body = {
+                "valueInputOption": "USER_ENTERED",
+                "data": [
+                    {"range": cell_vict,
+                     "values": empt_ll}]
+            }).execute()
+            elif name[0][0] == best_cycle[num] and num == (len(best_cycle) - 1):
+                empt_l = []
+                empt_ll = []
+                empt_l.append(best_cycle[0])
+                empt_ll.append(empt_l)
+                results = service.spreadsheets().values().batchUpdate(spreadsheetId = shit_id, body = {
+                    "valueInputOption": "USER_ENTERED",
+                    "data": [
+                    {"range": cell_vict,
+                     "values": empt_ll}]
+            }).execute()   
+    
 def id_check(id):
     ids = 'ДОСТАТЬ ИЗ ФАЙЛА'
     if id not in ids:
@@ -149,6 +272,7 @@ def org_quest(chat_id, text):
     elif text == 'Завершить прохождение опросов и начать игру':
         keyb_orgq = types.ReplyKeyboardRemove
         status_writer(chat_id, 'orggame')
+        separator(shit_id)
         ids = 'ДОСТАТЬ ИЗ ФАЙЛА'
         for some_id in ids:
             part_startgame(some_id)
