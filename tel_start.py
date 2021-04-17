4af10ae51938fe9c55ca3e0403b28a109cd72450
a.tulubieva <71927278+tuluqeva@users.noreply.github.com>
2021-04-18 00:00:57 +0400

    Update tel_start.py

--- tel_start.py
+++ tel_start.py
@@ -116,7 +116,7 @@ def separator(shit_id):
     final_cycles = []
     weighted_edges = []
     mutual_edges = []
-    limit = 0
+
     raw_graph = nx.DiGraph()
     raw_graph.add_edges_from(edges)
     raw_graph.add_nodes_from(isolat)
@@ -125,50 +125,50 @@ def separator(shit_id):
         rev_edge = list(edges[edge_number])
         rev_edge.reverse()
         if tuple(rev_edge) not in edges:
-             raw_graph.remove_edge(edges[edge_number][0], edges[edge_number][1])
+            raw_graph.remove_edge(edges[edge_number][0], edges[edge_number][1])
     paths = nx.shortest_path(raw_graph)
     no_conn_rate = len(nodes) + 1
     weighted_edges = []
-    for start in paths:
-        for target in nodes:
-            if start != target:
-                if target in paths[start]:
-                    weight = len(paths[start][target])
-                else:
-                    weight = no_conn_rate
-                if weight > limit:
-                    weighted_edge = []
-                    weighted_edge.append(start)
-                    weighted_edge.append(target)
-                    weighted_edge.append(weight)
-                    weighted_edges.append(tuple(weighted_edge))
-    weighted_graph = nx.DiGraph()
-    weighted_graph.add_weighted_edges_from(weighted_edges)
-    final_paths =  weighted_graph.adj
-    cycles = list(nx.simple_cycles(weighted_graph))
-    if len(cycles) == 0:
-        print('измените значение limit')
-    else:
-        for cycle in cycles:
-            if len(cycle) == len(nodes):
-                reversed = []
-                reversed.append(cycle[0])
-                part = cycle[1:len(cycle)]
-                part.reverse()
-                reversed.extend(part)
-                if reversed not in final_cycles:
-                    final_cycles.append(cycle)
-        max_count = 0
-        for cycle in final_cycles:
-            cycle_count = 0
-            for n_number in range(len(nodes) - 1):
-                start = cycle[n_number]
-                target = cycle[n_number + 1]
-                cycle_count += final_paths[start][target]['weight']
-            cycle_count += final_paths[cycle[-1]][cycle[0]]['weight']
-            if cycle_count > max_count:
-                max_count = cycle_count
-                best_cycle = cycle
+    for limit in range(4, 0, -1):
+        for start in paths:
+            for target in nodes:
+                if start != target:
+                    if target in paths[start]:
+                        weight = len(paths[start][target])
+                    else:
+                        weight = no_conn_rate
+                    if weight > limit:
+                        weighted_edge = []
+                        weighted_edge.append(start)
+                        weighted_edge.append(target)
+                        weighted_edge.append(weight)
+                        weighted_edges.append(tuple(weighted_edge))
+        weighted_graph = nx.DiGraph()
+        weighted_graph.add_weighted_edges_from(weighted_edges)
+        final_paths =  weighted_graph.adj
+        cycles = list(nx.simple_cycles(weighted_graph))
+        if weighted_graph.nodes == len(nodes) and len(cycles) != 0:
+            break
+    for cycle in cycles:
+        if len(cycle) == len(nodes):
+            reversed = []
+            reversed.append(cycle[0])
+            part = cycle[1:len(cycle)]
+            part.reverse()
+            reversed.extend(part)
+            if reversed not in final_cycles:
+                final_cycles.append(cycle)
+    max_count = 0
+    for cycle in final_cycles:
+        cycle_count = 0
+        for n_number in range(len(nodes) - 1):
+            start = cycle[n_number]
+            target = cycle[n_number + 1]
+            cycle_count += final_paths[start][target]['weight']
+        cycle_count += final_paths[cycle[-1]][cycle[0]]['weight']
+        if cycle_count > max_count:
+            max_count = cycle_count
+            best_cycle = cycle
 #запись в фаил
     for number in range(2,length+1):
         cell_name = column_name + str(number)
