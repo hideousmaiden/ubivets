58f74f37642f5fc742ca99c042680a6a5127a74a
hideousmaiden <71965957+hideousmaiden@users.noreply.github.com>
2021-05-16 22:32:50 +0300

    Update tel_start.py

--- tel_start.py
+++ tel_start.py
@@ -483,7 +483,7 @@ def gamereader(id):
 def photo_sender(m):
     file_info = bot.get_file(m.photo[len(m.photo) - 1].file_id)
     downloaded_file = bot.download_file(file_info.file_path)
-    src = "/home/cyparaber/ubivets" + file_info.file_path
+    src = "/home/cyparaber/ubivets/" + file_info.file_path
     with open(src, "wb") as new_file:
         new_file.write(downloaded_file)
     user_id = m.chat.id
